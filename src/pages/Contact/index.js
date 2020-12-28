215cb4a4ccd2920e163427df34eb5dcb8bbbb980
Murilo-V <alveiramurilo@hotmail.com>
2020-12-28 17:25:26 -0300

    feat: contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Menu from '../../components/Menu';
+import SvgGithubLogo from '../../assets/github-logo.svg'
+import SvgInstagramLogo from '../../assets/instagram-logo.svg'
+import SvgLinkedInLogo from '../../assets/linkedin-logo.svg'
 
 import { Container } from './styles';
 
@@ -7,6 +10,31 @@ const Contact = () => {
   return (
     <Container>
       <Menu />
+      <h1>contato</h1>
+      <div className="contact-container">
+        <div className="social-medias">
+            <a href="https://www.github.com/murilo-v" target="_blank" rel="noopener noreferrer">
+              <div>
+                <img src={SvgGithubLogo} alt="Github Logo" />
+                <p>acompanhe meus projetos no GitHub</p>
+              </div>
+            </a>
+
+            <a href="https://www.instagram.com/_murilov._/" target="_blank" rel="noopener noreferrer">
+              <div>
+                <img src={SvgInstagramLogo} alt="Instagram Logo" />
+                <p>tire suas dúvidas no Instagram</p>
+              </div>
+            </a>
+
+            <a href="https://www.linkedin.com/in/murilo-v" target="_blank" rel="noopener noreferrer">
+              <div>
+                <img src={SvgLinkedInLogo} alt="LinkedIn Logo" />
+                <p>faça networking comigo no LinkedIn</p>
+              </div>
+            </a> 
+        </div>
+      </div>
     </Container>
   );
 };
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,5 +1,99 @@
 import styled from 'styled-components';
+import SvgBackgroundContact from '../../assets/background-contact.svg';
+import SvgGreenLinkCursor from '../../assets/green-link-cursor.svg'
+
 
 export const Container = styled.div`
 
+    @media screen and (max-width: 800px) {
+        .contact-container {
+            .social-medias {
+                    flex-direction: column !important;
+            }
+        } 
+    }
+
+    @media screen and (max-width: 600px) {
+        h1 {
+            font-size: 36pt !important;
+            padding-top: 70px !important;
+        }
+
+        .contact-container {
+
+            .social-medias {
+                div {
+                    margin: 15px !important;
+                    img {
+                        max-width: 50px !important;
+                    }
+
+                    p {
+                        font-size: 14pt !important;
+                        line-height: 20px !important;
+                    }
+                }
+            }
+        } 
+    }
+
+    h1 {
+        font-size: 40pt;
+        line-height: 74px;
+        color: #D5DBD5;
+        text-shadow: 3px 3px 0px #3F8D1A;
+        width: 100vw !important;
+        text-align: center !important;
+        position: fixed;
+        padding-top: 90px;
+        font-weight: 500;
+    }
+
+    .contact-container {
+        width: 100vw;
+        height: 100vh;
+        background-image: url(${SvgBackgroundContact});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        .social-medias {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            height: 100%;
+            
+
+            a {
+                text-decoration: none;
+                cursor: url(${SvgGreenLinkCursor}), default;
+            }
+
+            div {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                margin: 30px;
+                cursor: url(${SvgGreenLinkCursor}), default;
+
+                img {
+                    max-width: 70px;
+                    margin-bottom: 10px;
+                    cursor: url(${SvgGreenLinkCursor}), default;
+                }
+
+                p {
+                    color: #D5DBD5;
+                    font-weight: 500;
+                    line-height: 30px;
+                    text-align: center;
+                    font-size: 16pt;
+                    width: 220px;
+                    cursor: url(${SvgGreenLinkCursor}), default;
+                }
+            }
+        }
+    }
 `;
